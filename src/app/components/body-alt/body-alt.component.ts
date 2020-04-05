18b1b72ffe0e78088834006955f3cca9330efe88
Adam Callanan <a.callanan@sbcglobal.net>
2020-04-04 23:29:12 -0500

    start body alt

---
+++ src/app/components/body-alt/body-alt.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body-alt',
+  templateUrl: './body-alt.component.html',
+  styleUrls: ['./body-alt.component.scss']
+})
+export class BodyAltComponent {
+}
