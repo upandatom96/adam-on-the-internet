f04ab5936f28c86d794b00e484aa315c4177d2a3
Adam Callanan <a.callanan@sbcglobal.net>
2020-04-20 21:55:21 -0500

    try without ssl

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const forceSSL = function () {
   return function (req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
       return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
+        ['http://', req.get('Host'), req.url].join('')
       );
     }
     next();
