237e4bd04857921af87be70db3a2741e415217de
Adam Callanan <a.callanan@sbcglobal.net>
2020-04-21 21:53:22 -0500

    better resources

--- src/app/components/resources/resources.component.ts
+++ src/app/components/resources/resources.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, } from "@angular/core";
+import { Resource } from 'src/app/models/Resource.model';
+
+// tslint:disable: max-line-length
 
 @Component({
   selector: "app-resources",
   templateUrl: "./resources.component.html",
   styleUrls: ["./resources.component.scss"]
 })
-export class ResourcesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class ResourcesComponent {
+  public resources: Resource[] = [
+    {
+      name: "Angular (2+)",
+      details: "Most of my UIs are created using Angular.",
+    },
+    {
+      name: "Node w/ Express",
+      details: "Node w/ Express powers my API, connecting to my MongoDB via Mongoose. I also use a simple Express app to serve my Angular apps with Heroku.",
+    },
+    {
+      name: "MongoDb w/ Atlas",
+      details: "I use a Mongo Database to store my data. I use a cloud database with Mongo Atlas.",
+    },
+    {
+      name: "Bootstrap w/ Font Awesome",
+      details: "For styling most of my applications, I use Bootstrap 4 and the Font Awesome version that it includes.",
+    },
+    {
+      name: "Heroku",
+      details: "I use the free tier of Heroku for hosting my web applications.",
+    },
+    {
+      name: "GitHub",
+      details: "I use GitHub for remote repositories for my code.",
+    },
+    {
+      name: "Metro Studio",
+      details: "I use the free application Metro Studio to generate Favicons for my applications.",
+    },
+    {
+      name: "Travis CI",
+      details: "I use the free tier of Travis CI to automatically test most of my applications before they deploy.",
+    },
+  ];
 
 }
---
+++ src/app/models/Resource.model.ts
@@ -0,0 +1,4 @@
+export interface Resource {
+  name: string;
+  details: string;
+}
